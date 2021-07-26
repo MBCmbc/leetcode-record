82aca82c84f3b4a274a31dc830b89a57e441b8d0
MBCmbc <806166563@qq.com>
2021-07-26 20:53:22 +0800

    commit

--- src/main/java/JzOffer/P60/Solution.java
+++ src/main/java/JzOffer/P60/Solution.java
@@ -11,18 +11,18 @@ https://leetcode-cn.com/problems/nge-tou-zi-de-dian-shu-lcof/solution/java-dong-
 */
 
 class Solution {
-	public double[] dicesProbability(int n) {
-		double[] pre = new double[]{1/6d, 1/6d, 1/6d, 1/6d, 1/6d, 1/6d};    //最初的pre，也即1个骰子的情况
-		for(int i = 2; i <= n; i++){    //循环推算i个骰子的情况，直到最终n个骰子的情况
-			double[] tmp = new double[i * 5 + 1];   //tmp代表本轮要计算的数组，即i个骰子的情况。i为骰子个数，长度为6*i-1*i+1 = 5*i+1
-			for(int j = 0; j < pre.length; j++){    //循环计算i-1个骰子（j为下标）情况下，再加一个骰子（x为下标），形成的i个骰子的数组。
-				for(int x = 0; x < 6; x++) {
-					tmp[j+x] += pre[j]/6;           //根据状态转移公式得出。    1个骰子情况下概率均为1/6，所以直接除以6即可。
-				}
-			}
-			pre = tmp;
-		}
+    public double[] dicesProbability(int n) {
+        double[] pre = new double[]{1 / 6d, 1 / 6d, 1 / 6d, 1 / 6d, 1 / 6d, 1 / 6d};    //最初的pre，也即1个骰子的情况
+        for (int i = 2; i <= n; i++) {    //循环推算i个骰子的情况，直到最终n个骰子的情况
+            double[] tmp = new double[i * 5 + 1];   //tmp代表本轮要计算的数组，即i个骰子的情况。i为骰子个数，长度为6*i-1*i+1 = 5*i+1
+            for (int x = 0; x < pre.length; x++) {    //循环计算i-1个骰子（j为下标）情况下，再加一个骰子（x为下标），形成的i个骰子的数组。
+                for (int y = 0; y < 6; y++) {
+                    tmp[x + y] += pre[x] / 6d;           //根据状态转移公式得出。    1个骰子情况下概率均为1/6，所以直接除以6即可。
+                }
+            }
+            pre = tmp;
+        }
 
-		return pre;
-	}
+        return pre;
+    }
 }
